dedc050b25eddc7c8cfeb1e106cac977d9e58cbe
azmiblidaoui <azmi.blidaoui@gmail.com>
2023-03-21 22:42:35 +0100

    class linked_list

--- Simple Linked List /Linked_List.py
+++ Simple Linked List /Linked_List.py
@@ -2,3 +2,7 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.Next = None #Pointer to Node
+class linled_List:
+    def __init__(self):
+        self.head = None #Pointer to first node
+    
