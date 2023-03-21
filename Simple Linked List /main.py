892012ee9f9b58874d90fc6b0da99a823629601f
azmiblidaoui <azmi.blidaoui@gmail.com>
2023-03-21 23:04:17 +0100

    working with linked list

---
+++ Simple Linked List /main.py
@@ -0,0 +1,10 @@
+from Linked_List import linled_List
+
+l1 = linled_List()
+l1.add_last_list(1)
+l1.add_last_list(2)
+l1.add_last_list(3)
+l1.add_last_list(4)
+l1.add_last_list(5)
+l1.add_front_list(6)
+l1.print_List()
