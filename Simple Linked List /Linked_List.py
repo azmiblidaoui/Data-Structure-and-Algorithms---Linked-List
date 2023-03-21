a053c11ed6fb1212d0158da4dab4c69ebfc4fa66
azmiblidaoui <azmi.blidaoui@gmail.com>
2023-03-21 22:50:02 +0100

    crearte fun add_last_list

--- Simple Linked List /Linked_List.py
+++ Simple Linked List /Linked_List.py
@@ -5,4 +5,14 @@ class Node:
 class linled_List:
     def __init__(self):
         self.head = None #Pointer to first node
-    
+    def add_last_list(self,data):
+        new_Node = Node(data)
+        if self.head is None:
+            self.head = new_Node
+        else:
+            curr = self.head
+            while curr.Next is not None:
+                if curr.Next is None:
+                    break
+                curr = curr.Next
+            curr.Next = new_Node
